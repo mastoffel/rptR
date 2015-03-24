534f3c9a75770990c13b76708fb932efdae86605
Thinkmarti <martin.adam.stoffel@gmail.com>
2015-03-24 16:06:33 +0100

    added package doc

---
+++ R/rptR.R
@@ -0,0 +1,9 @@
+#' rptR: A package for calculating repeatabilities for gaussian and non-gaussian data.
+#'
+#' Click on index at the bottom of the page to see all the functions!
+#' 
+#' 
+#'
+#' @docType package
+#' @name rptR
+NULL
