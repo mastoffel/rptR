5a376dbe4b855b88d8f935028cc2023229e84e60
Holger Schielzeth <holger.schielzeth@uni-jena.de>
2016-07-30 23:08:30 +0200

    Fixed further issues in rptGaussian documentation

--- R/rptGaussian.R
+++ R/rptGaussian.R
@@ -1,45 +1,50 @@
-#' GLMM-based Repeatability Using REML
+#' GLMM-based Repeatability Using REML for Gaussian data
 #' 
-#' Calculates repeatability from a general linear mixed-effects models fitted by REML (restricted maximum likelihood).
-#' @param formula Formula as used e.g. by \link{glmer}. The grouping factor(s) of
+#' Estimates the repeatability from a general linear mixed-effects models fitted by restricted maximum likelihood (REML).
+#' @param formula Formula as used e.g. by \link{lmer}. The grouping factor(s) of
 #'        interest needs to be included as a random effect, e.g. '(1|groups)'.
-#'        Covariates and additional random effects can be included to estimate adjusted repeatabilities.
+#'        Covariates and additional random effects can be included to estimate adjusted 
+#'        repeatabilities.
 #' @param grname A character string or vector of character strings giving the
 #'        name(s) of the grouping factor(s), for which the repeatability should
-#'        be estimated. Spelling needs to match the random effect names as given in \code{formula}.
+#'        be estimated. Spelling needs to match the random effect names as given in \code{formula} 
+#'        and terms have to be set in quotation marks.
 #' @param data A dataframe that contains the variables included in the \code{formula}
 #'        and \code{grname} arguments.
-#' @param CI Width of the confidence interval (defaults to 0.95).
+#' @param CI Width of the required confidence interval (defaults to 0.95).
 #' @param nboot Number of parametric bootstraps for interval estimation.
-#'        Defaults to 1000. Larger numbers of permutations give a better
-#'        asymtotic CI, but may be very time-consuming.
-#' @param npermut Number of permutations used when calculating 
-#'        asymptotic \emph{P} values (defaults to 1000). 
-#' @param parallel If TRUE, bootstraps will be distributed. 
+#'        Defaults to 1000. Larger numbers of bootstraps give a better
+#'        asymtotic CI, but may be very time-consuming (in particular of some variance component 
+#'        is low). Bootstrapping can be switch off by setting \code{nboot = 0}.
+#' @param npermut Number of permutations used when calculating asymptotic \emph{P} 
+#'        values (defaults to 1000). Larger numbers of permutations give a better
+#'        asymtotic CI, but may be very time-consuming (in particular of some variance component 
+#'        is low). Permutaton tests can be switch off by setting \code{npermut = 0}. 
+#' @param parallel If TRUE, bootstraps and permutations will be distributed across multiple cores. 
 #' @param ncores Specify number of cores to use for parallelization. On default,
 #'        all cores but one are used.
 #' 
 #' @return 
-#' Returns an object of class rpt that is a a list with the following elements: 
-#' \item{call}{function call}
-#' \item{datatype}{Response distribution (here: 'Gaussian')}.
-#' \item{CI}{Width of the confidence interval.}
-#' \item{R}{\code{data.frame} with point estimates for repeatabilities. Columns
-#'      are groups of interest. Rows are original and link scale, in this order.}
+#' Returns an object of class \code{rpt} that is a a list with the following elements: 
+#' \item{call}{Function call}
+#' \item{datatype}{Response distribution (here: 'Gaussian').}
+#' \item{CI}{Coverage of the confidence interval as specified by the \code{CI} argument.}
+#' \item{R}{\code{data.frame} with point estimates for repeatabilities for each grouping factor
+#'       of interest.}
 #' \item{se}{\code{data.frame} with approximate standard errors (\emph{se}) for repeatabilities. 
-#'      Grouping factors are rows.
-#'      Note that the distribution might not be symmetrical, in which case the \emph{se} is less informative.}
-#' \item{CI_emp}{\code{data.frame} containing the confidence intervals for the repeatabilities from
-#'      parametric bootstrapping. Each row is a grouping factor of interest.}
-#' \item{P}{Approximate \emph{P} \code{data.frame} with p-values from a significance test based on likelihood-ratio
-#'      in the first column and significance test based on permutation of residuals in the second column. 
-#'      Each row is a grouping factor.}
-#' \item{R_boot}{Parametric bootstrap samples for \emph{R}. Each \code{list}
-#'       element is a grouping factor.}
-#' \item{R_permut}{Permutation samples for \emph{R}. Each \code{list}
-#'       element is a grouping factor.}
-#' \item{LRT}{List of Likelihood-ratios for the model and the reduced model(s), 
-#'       and \emph{P} value(s) and degrees of freedom for the Likelihood-ratio test} 
+#'      Rows repsresent grouping factors of interest. Note that the distribution might not be symmetrical, 
+#'      in which case the \emph{se} is less informative.}
+#' \item{CI_emp}{\code{data.frame} containing the (empirical) confidence intervals for the repeatabilities 
+#'      estiamted based parametric bootstrapping. Each row represents a grouping factor of interest.}
+#' \item{P}{\code{data.frame} with approximate p-values based on likelihood-ratio tests
+#'      (first column) and permutation tests (second column). Each row represents a grouping factor 
+#'      of interest.}
+#' \item{R_boot}{Vector(s) of parametric bootstrap samples for \emph{R}. Each \code{list}
+#'       element respesents a grouping factor.}
+#' \item{R_permut}{Vector(s) of permutation samples for \emph{R}. Each \code{list}
+#'       element represents a grouping factor.}
+#' \item{LRT}{List of likelihoods for the full model and the reduced model(s), likelihood ratios \emph(D), 
+#'      \emph{P} value(s) and degrees of freedom for the likelihood-ratio test.} 
 #' \item{ngroups}{Number of groups.}
 #' \item{nobs}{Number of observations.}
 #' \item{mod}{Fitted model.}
