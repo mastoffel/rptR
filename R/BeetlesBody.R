37e97d304dc858d870e30fd0dc1d27c963a933fb
Holger Schielzeth <holger.schielzeth@uni-jena.de>
2016-08-04 23:50:13 +0200

    Style and reference updates in documentation of datasets

--- R/BeetlesBody.R
+++ R/BeetlesBody.R
@@ -1,11 +1,13 @@
 #' BeetlesBody dataset
 #' 
-#' @details This is an example dataset which was used as a simulated toy dataset for a different purpose 
+#' @details This is an simulated dataset which was used as a toy example for a different purpose 
 #' (Nakagawa & Schielzeth 2013). 
-#' It offers a balanced dataset with rather simple structure and sizable effects. 
+#' It offers a balanced dataset with rather simple structure, sizable effects and decent sample size, 
+#' just right for demonstrating some features of \code{rptR}. 
 #' Sufficient sample size is required in particular for the non-Gaussian traits, 
 #' because those tend to be more computationally demanding and less rich in information per data 
 #' point than simple Gaussian traits.
+#' 
 #' In brief the imaginary sampling design of the simulated dataset is as follows. 
 #' Beetle larvae were sampled from 12 populations (`Population`) with samples taken from two 
 #' discrete microhabitats at each location (`Habitat`). Samples were split in equal proportion 
@@ -14,8 +16,8 @@
 #' in this dataset is body length (`BodyL`).
 #' 
 #' @references 
-#' Nakagawa, S. & H. Schielzeth (2013). A general and simple method for obtaining R2
-#' from generalized linear mixed-effects models. Methods in Ecology and Evolution 4: 133-142.
+#' Nakagawa, S. & Schielzeth, H. (2013) \emph{A general and simple method for obtaining R2
+#' from generalized linear mixed-effects models}. Methods in Ecology and Evolution 4: 133-142.
 #' 
 #'       
 #' @keywords datasets
