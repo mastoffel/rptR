e5464e1d8ce39bd9b48853c9a1f698d55909d7c8
Martin Stoffel <martin.adam.stoffel@gmail.com>
2016-08-04 16:37:38 +0200

    examples for rpt added

--- R/rpt.R
+++ R/rpt.R
@@ -48,6 +48,37 @@
 #'         Martin A. Stoffel (martin.adam.stoffel@@gmail.com)
 #' @seealso \link{rptR}
 #' 
+#' @examples
+#' # load data
+#' data(BeetlesBody)
+#' data(BeetlesMale)
+#' data(BeetlesFemale)
+#'
+#' #  prepare proportion data
+#' BeetlesMale$Dark <- BeetlesMale$Colour
+#' BeetlesMale$Reddish <- (BeetlesMale$Colour-1)*-1
+#' md <- aggregate(cbind(Dark, Reddish) ~ Population + Container, data=BeetlesMale, FUN=sum)
+#' 
+#' 
+#' # Note: all function calls are run without bootstraps and permutation for speed reasons
+#' 
+#' # gaussian data
+#' rpt(BodyL ~ (1|Population), grname="Population", data=BeetlesBody, 
+#' nboot=0, npermut=0, datatype = "Gaussian"
+#' 
+#' # poisson data
+#' rpt(Egg ~ Treatment + (1|Container), grname=c("Container"), data = BeetlesFemale,
+#' nboot=0, npermut=0, datatype = "Poisson")
+#' 
+#' # binary data
+#' rpt(Colour ~ (1|Population), grname=c("Population"), 
+#' data=BeetlesMale, nboot=5, npermut=0, datatype = "Binary")
+#' 
+#' # proportion data
+#' rpt(cbind(Dark, Reddish) ~ (1|Population), grname=c("Population"), data=md,
+#' nboot=0, npermut=0, datatype = "Proportion")
+#' 
+#' 
 #' @keywords models
 #' 
 #' @export
