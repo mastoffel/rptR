366fade2d0f7881a37b2d069724d3db80fa99f91
Martin Stoffel <martin.adam.stoffel@gmail.com>
2016-08-22 11:53:05 +0200

    adjusted unit tests after bug correction in rpt estimates with multiple random effects

--- tests/testthat/test-rptPoisson.R
+++ tests/testthat/test-rptPoisson.R
@@ -83,18 +83,18 @@ test_that("rpt estimation works for one random effect, no boot, permut, no paral
 
 ########## checks for two random effects
 
-# run with one random effect, no boot, no permut
+# run with two random effect, no boot, no permut
 R_est_1 <- rptPoisson(Egg ~ Treatment + (1|Container) + (1|Population), grname=c("Container", "Population"), data = BeetlesFemale,
         nboot=0, npermut=0)
 
 test_that("rpt estimation works for two random effect, no boot, no permut, no parallelisation, logit link", {
         expect_that(is.numeric(unlist(R_est_1$R)), is_true()) 
         # 1st random effect
-        expect_equal(R_est_1$R["R_org", 1], 0.05716180, tolerance = 0.001)
-        expect_equal(R_est_1$R["R_link", 1], 0.06018504, tolerance = 0.001)
+        expect_equal(R_est_1$R["R_org", 1], 0.02961663, tolerance = 0.001)
+        expect_equal(R_est_1$R["R_link", 1], 0.03301502, tolerance = 0.001)
         # 2nd random effect
-        expect_equal(R_est_1$R["R_org", 2], 0.4881728, tolerance = 0.001)
-        expect_equal(R_est_1$R["R_link", 2], 0.4668547, tolerance = 0.001)
+        expect_equal(R_est_1$R["R_org", 2], 0.4680674, tolerance = 0.001)
+        expect_equal(R_est_1$R["R_link", 2], 0.4514415, tolerance = 0.001)
 })
 
 test_that("LRTs works", {
@@ -104,7 +104,7 @@ test_that("LRTs works", {
 })
 
 
-# run with one random effect, boot, no permut
+# run with two random effect, boot, no permut
 R_est_2 <- rptPoisson(Egg ~ Treatment + (1|Container) + (1|Population), grname=c("Container", "Population"), data = BeetlesFemale,
         nboot=2, npermut=0)
 
@@ -112,32 +112,32 @@ test_that("rpt estimation works for two random effect, boot, no permut, no paral
         
         # original scale
         # 1st random effect
-        expect_equal(as.numeric(R_est_2$CI_emp$CI_org[1, "2.5%"]),  0.02544666, tolerance = 0.001)
-        expect_equal(as.numeric(R_est_2$CI_emp$CI_org[1, "97.5%"]), 0.05610622, tolerance = 0.001)
+        expect_equal(as.numeric(R_est_2$CI_emp$CI_org[1, "2.5%"]),  0.02038247, tolerance = 0.001)
+        expect_equal(as.numeric(R_est_2$CI_emp$CI_org[1, "97.5%"]), 0.02425887, tolerance = 0.001)
         # 2nd random effect
-        expect_equal(as.numeric(R_est_2$CI_emp$CI_org[2, "2.5%"]), 0.20203285 , tolerance = 0.001)
-        expect_equal(as.numeric(R_est_2$CI_emp$CI_org[2, "97.5%"]), 0.57533006, tolerance = 0.001)
+        expect_equal(as.numeric(R_est_2$CI_emp$CI_org[2, "2.5%"]),  0.1976287 , tolerance = 0.001)
+        expect_equal(as.numeric(R_est_2$CI_emp$CI_org[2, "97.5%"]),0.5535698, tolerance = 0.001)
         
         
         # link scale
         # 1st random effect
-        expect_equal(as.numeric(R_est_2$CI_emp$CI_link[1, "2.5%"]),  0.02747585, tolerance = 0.001)
-        expect_equal(as.numeric(R_est_2$CI_emp$CI_link[1, "97.5%"]),0.05960882, tolerance = 0.001)
+        expect_equal(as.numeric(R_est_2$CI_emp$CI_link[1, "2.5%"]), 0.02166587, tolerance = 0.001)
+        expect_equal(as.numeric(R_est_2$CI_emp$CI_link[1, "97.5%"]),0.02815924, tolerance = 0.001)
         # 2nd random effect
-        expect_equal(as.numeric(R_est_2$CI_emp$CI_link[2, "2.5%"]),  0.20581645, tolerance = 0.001)
-        expect_equal(as.numeric(R_est_2$CI_emp$CI_link[2, "97.5%"]), 0.53812845, tolerance = 0.001)
+        expect_equal(as.numeric(R_est_2$CI_emp$CI_link[2, "2.5%"]),  0.201299, tolerance = 0.001)
+        expect_equal(as.numeric(R_est_2$CI_emp$CI_link[2, "97.5%"]), 0.5229169, tolerance = 0.001)
         
 })
 
 
 
-# run with one random effect, no boot, permut
+# run with two random effect, no boot, permut
 R_est_3 <- rptPoisson(Egg ~ Treatment + (1|Container) + (1|Population), grname=c("Container", "Population"), data = BeetlesFemale,
         nboot=0, npermut=5)
 
 test_that("rpt estimation works for two random effect, no boot, permut, no parallelisation, logit link", {
         # original scale
-        expect_equal(R_est_3$P$P_permut_org[1], 0.8, tolerance = 0.001)
+        expect_equal(R_est_3$P$P_permut_org[1], 1, tolerance = 0.001)
         expect_equal(R_est_3$P$P_permut_org[2], 0.2, tolerance = 0.001)
         # link scale
         expect_equal(R_est_3$P$P_permut_link[1], 1, tolerance = 0.001)
