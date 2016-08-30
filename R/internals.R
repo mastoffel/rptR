8759a5bab32241159c5387871c9418e28bfe8975
Martin Stoffel <martin.adam.stoffel@gmail.com>
2016-08-30 17:36:24 +0200

    changed .with_warnings back to with_warnings including documentation

--- R/internals.R
+++ R/internals.R
@@ -1,15 +1,15 @@
-# Captures and suppresses (still to find out why) warnings of an expression
-# 
-# This function is used within rptR to capture lme4 model fitting warnings in the
-# bootstrap and permutation procedures.
-# 
-# An expression, such as the sequence of code used by rptR to calculate
-# bootstrap or permutation estimates
-# 
-# 
+#' Captures and suppresses (still to find out why) warnings of an expression
+#' 
+#' This function is used within rptR to capture lme4 model fitting warnings in the
+#' bootstrap and permutation procedures.
+#' 
+#' @param An expression, such as the sequence of code used by rptR to calculate
+#' bootstrap or permutation estimates
+#' 
+#' @keywords internal 
 
 
-.with_warnings <- function(expr) {
+with_warnings <- function(expr) {
         myWarnings <- NULL
         wHandler <- function(w) {
                 myWarnings <<- c(myWarnings, list(w))
