7ed3559660019836cec27e78cbf3ae5ab6059d0b
Schielzeth <Schielzeth>
2017-02-28 18:21:31 +0100

    'update' and 'rptObj' now handed over by rpt

--- R/rpt.R
+++ R/rpt.R
@@ -157,22 +157,22 @@ rpt <- function(formula, grname, data, datatype = c("Gaussian", "Binomial", "Pro
     rptObj = NULL, update = FALSE) {
         
     if (datatype == "Gaussian") {
-            out_gaussian <- rptGaussian(formula, grname, data, CI, nboot, npermut, parallel, ncores, ratio, adjusted)
+            out_gaussian <- rptGaussian(formula, grname, data, CI, nboot, npermut, parallel, ncores, ratio, adjusted, rptObj, update)
             out_gaussian$call <- match.call()
             return(out_gaussian)
     }
     if (datatype == "Binary") {
-            out_binary <- rptBinary(formula, grname, data, link, CI, nboot, npermut, parallel, ncores, ratio, adjusted, expect)
+            out_binary <- rptBinary(formula, grname, data, link, CI, nboot, npermut, parallel, ncores, ratio, adjusted, expect, rptObj, update)
             out_binary$call <- match.call()
             return(out_binary)
     }
     if (datatype == "Proportion") {
-            out_proportion <- rptProportion(formula, grname, data, link, CI, nboot, npermut, parallel, ncores, ratio, adjusted, expect)
+            out_proportion <- rptProportion(formula, grname, data, link, CI, nboot, npermut, parallel, ncores, ratio, adjusted, expect, rptObj, update)
             out_proportion$call <- match.call()
             return(out_proportion)
     }
     if (datatype == "Poisson") {
-            out_poisson <- rptPoisson(formula, grname, data, link, CI, nboot, npermut, parallel, ncores, ratio, adjusted, expect)
+            out_poisson <- rptPoisson(formula, grname, data, link, CI, nboot, npermut, parallel, ncores, ratio, adjusted, expect, rptObj, update)
             out_poisson$call <- match.call()
             return(out_poisson)
     }
